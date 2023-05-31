232939e4e22cc0b9a55e42341f655b5c0739c02b
Julio Castro <julio.castro.dev@gmail.com>
2023-05-31 23:57:05 +0200

    chore: clean up default configs

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import "./Home.scss"
+
+export const Home = () => {
+  return (
+    <main class="container">
+      <h1>Home</h1>
+      <p>First hello world</p>
+      <p class="red">Second hello world</p>
+      <p>Third hello world</p>
+    </main>
+  )
+}
